4a7084a67d94c361222b2d74204b13159373b664
Ghabi <ghabiassaad@gmail.com>
2020-04-11 01:31:44 +0100

    Add project files.

---
+++ OrchadCoreCMSModuleLibrary/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace OrchadCoreCMSModuleLibrary
+{
+    public class Startup
+    {
+        public void Configure(IEndpointRouteBuilder endpoint)
+        {
+            endpoint.MapGet("/library", async context =>
+            {
+                await context.Response.WriteAsync("Modular Library!");
+            });
+        }
+    }
+}
---
+++ OrchardCoreCMSWeb/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OrchardCore.Logging;
+
+namespace OrchardCoreCMSWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging => logging.ClearProviders())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    .UseNLogWeb();
+                });
+    }
+}
